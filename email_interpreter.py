3a666ca59b5a1a6edf638f2c10178e6411c5eafc
HellsMayo <39375961+HellsMayo@users.noreply.github.com>
2019-07-03 12:39:41 -0400

    Add files via upload

---
+++ email_interpreter.py
@@ -0,0 +1,155 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+import email
+import datetime
+
+import string_key
+from send_webhooks import SendWebhooks
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+
+def main():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    #response = service.users().labels().list(userId='me').execute()
+    #labels = response['labels']
+    #for label in labels:
+    #    print('Label id: %s - Label name: %s' % (label['id'], label['name']))
+
+    # Call the Gmail API to return all unread emails
+    results = service.users().messages().list(userId='me', labelIds='UNREAD').execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print('No unread messages found.')
+    else:
+        unread_new_turns = []
+
+        for message in messages:
+            # create a dictionary object reresenting the email
+            msg = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
+
+            # retreave raw byte data from dictionary object
+            msg_bytes = base64.urlsafe_b64decode(msg['raw'].encode('ASCII'))
+
+            # create message object from byte data
+            mime_msg = email.message_from_bytes(msg_bytes)
+
+            # if the subject of the message is not the expected subject, do not pull data from it
+            if not mime_msg['Subject'] == string_key.subject:
+                break
+
+            # iterate over parts of the email object and store only the plain text of the email
+            if not mime_msg.is_multipart():
+                # retreave text of email part
+
+                text = mime_msg.get_payload()
+                #print(text)
+
+                # store dictionary returned from pull_data in unread_new_turns
+                new_turn = pull_data(text)
+                unread_new_turns.append(new_turn)
+                #print(new_turn)
+
+                # mark processed email as read
+                mark_as_read(service, message['id'])
+            else:
+                for part in mime_msg.walk():
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get('Content-Disposition'))
+
+                    # do not pull data from email part if it is not plain text with no attachments
+                    if content_type == 'text/plain' and 'attachment' not in content_disposition:
+                        # retreave text of email part
+                        text = part.get_payload()
+                        #print(text)
+
+                        # store dictionary returned from pull_data in unread_new_turns
+                        new_turn = pull_data(text)
+                        unread_new_turns.append(new_turn)
+                        #print(new_turn)
+
+                        # mark processed email as read
+                        mark_as_read(service, message['id'])
+                        break
+
+            #print("xxxxxxxxxxxxxxxx***next email***xxxxxxxxxxxxxxxx")
+
+        if unread_new_turns:
+            # create SendWebhook object and pass it all extracted data
+            sender = SendWebhooks(unread_new_turns)
+
+            # initiate send_all_new_turns and store boolean in completed
+            complete = sender.send_all_new_turns()
+
+            if complete:
+                print("All webhooks sent at %s." % datetime.datetime.now())
+            else:
+                print("Not all webhooks were not sent at %s." % datetime.datetime.now())
+        else:
+            print("No new turns to send at %s." % datetime.datetime.now())
+
+
+# returns dictionary data after given fields
+# formatted as {string_key.when:email_time,
+#               string_key.game:game_name,
+#               string_key.player:player_name,
+#               string_key.turn:turn_number}
+def pull_data(str):
+    fields_to_find = [string_key.when, string_key.game, string_key.player, string_key.turn]
+    found_data = dict()
+    i = 0
+    for field in fields_to_find:
+        # find index of the field starting at last point in the email string
+        # raise an error if the field is not found
+        try:
+            i = str.index(field, i)
+        except ValueError:
+            raise ValueError("Field not found within string after given starting point")
+        # set index to the first character after the field
+        i = i + len(field)
+        # store the string after the field and store it within found_data with the field as the key
+        data = ""
+        while not str[i] == '\r':
+            data += str[i]
+            i += 1
+        found_data[field] = data
+    # raise an error if found_data is not the same size as fields_to_find
+    assert (len(found_data) == len(fields_to_find))
+    return found_data
+
+
+# mark email with passed id as read
+def mark_as_read(service, msg_id):
+    editing_labels = {'addLabelIds': [], 'removeLabelIds': ['UNREAD']}
+    service.users().messages().modify(userId='me', id=msg_id, body=editing_labels).execute()
+
+
+if __name__ == '__main__':
+    main()
---
+++ send_webhooks.py
@@ -0,0 +1,55 @@
+import csv
+import requests
+from discord import Webhook, RequestsWebhookAdapter
+
+import string_key
+
+
+class SendWebhooks:
+    def __init__(self, new_turns):
+        assert(not len(new_turns) == 0)
+        self.new_turns = new_turns
+
+    def send_all_new_turns(self):
+        # pull discord player names for each steam player name from a local text file
+        player_aliases = {}
+        for row in parse_alias_file(string_key.path_to_player_aliases, '|'):
+            player_aliases[row[0]] = row[1]
+        print(player_aliases)
+
+        # pull webhook address for each game from a local text file
+        game_aliases = {}
+        for row in parse_alias_file(string_key.path_to_game_aliases, '|'):
+            game_aliases[row[0]] = {'id': row[1], 'token': row[2]}
+        print(game_aliases)
+
+        for turn in self.new_turns:
+            print(turn)
+
+            player_name = turn[string_key.player]
+            game_name = turn[string_key.game]
+            turn_number = turn[string_key.turn]
+
+            if player_name in player_aliases:
+                discord_name = player_aliases[player_name]
+            else:
+                discord_name = "[name unavailable]"
+
+            if game_name in game_aliases:
+                webhook_id = game_aliases[game_name]['id']
+                webhook_token = game_aliases[game_name]['token']
+            else:
+                webhook_id = game_aliases['Overflow']['id']
+                webhook_token = game_aliases['Overflow']['token']
+
+            webhook = Webhook.partial(webhook_id, webhook_token, adapter=RequestsWebhookAdapter())
+            webhook.send("<@%s>, take turn #%s in %s" % (discord_name, turn_number, game_name))
+        return False
+
+
+def parse_alias_file(path, delimiter=','):
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=delimiter)
+        for row in csv_reader:
+            if row:
+                yield row
