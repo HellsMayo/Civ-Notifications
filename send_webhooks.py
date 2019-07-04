d00eb9acb353371b418cba912aca5dba60f2e269
Benjamin Hellmann <benjaminhellmann42@gmail.com>
2019-07-03 20:30:33 -0400

    Removed print statements and fixed send_all_new_turns() return

--- send_webhooks.py
+++ send_webhooks.py
@@ -11,40 +11,40 @@ class SendWebhooks:
         self.new_turns = new_turns
 
     def send_all_new_turns(self):
-        # pull discord player names for each steam player name from a local text file
+        # pull discord player ids for each steam player name from a local text file
         player_aliases = {}
         for row in parse_alias_file(global_constants.path_to_player_aliases, '|'):
             player_aliases[row[0]] = row[1]
-        print(player_aliases)
 
-        # pull webhook address for each game from a local text file
+        # pull webhook ids and tokens to reach the channel's webhook bot for each game from a local text file
         game_aliases = {}
         for row in parse_alias_file(global_constants.path_to_game_aliases, '|'):
             game_aliases[row[0]] = {'id': row[1], 'token': row[2]}
-        print(game_aliases)
 
-        for turn in self.new_turns:
-            print(turn)
+        try:
+            for turn in self.new_turns:
+                player_name = turn[global_constants.player]
+                game_name = turn[global_constants.game]
+                turn_number = turn[global_constants.turn]
 
-            player_name = turn[global_constants.player]
-            game_name = turn[global_constants.game]
-            turn_number = turn[global_constants.turn]
+                if player_name in player_aliases:
+                    discord_name = player_aliases[player_name]
+                else:
+                    discord_name = "[name unavailable]"
 
-            if player_name in player_aliases:
-                discord_name = player_aliases[player_name]
-            else:
-                discord_name = "[name unavailable]"
+                if game_name in game_aliases:
+                    webhook_id = game_aliases[game_name]['id']
+                    webhook_token = game_aliases[game_name]['token']
+                else:
+                    webhook_id = game_aliases[global_constants.overflow_channel_name]['id']
+                    webhook_token = game_aliases[global_constants.overflow_channel_name]['token']
 
-            if game_name in game_aliases:
-                webhook_id = game_aliases[game_name]['id']
-                webhook_token = game_aliases[game_name]['token']
-            else:
-                webhook_id = game_aliases['Overflow']['id']
-                webhook_token = game_aliases['Overflow']['token']
-
-            webhook = Webhook.partial(webhook_id, webhook_token, adapter=RequestsWebhookAdapter())
-            webhook.send("<@%s>, take turn #%s in %s" % (discord_name, turn_number, game_name))
-        return False
+                webhook = Webhook.partial(webhook_id, webhook_token, adapter=RequestsWebhookAdapter())
+                webhook.send("<@%s>, take turn #%s in %s" % (discord_name, turn_number, game_name))
+        except Exception as error:
+            print(error)
+            return False
+        return True
 
 
 def parse_alias_file(path, delimiter=','):
