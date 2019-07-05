f0c656905266211f841895427269dd39fcedfb71
Benjamin Hellmann <benjaminhellmann42@gmail.com>
2019-07-05 16:44:23 -0400

    Remove comments from development and standardize print statmets

--- email_interpreter.py
+++ email_interpreter.py
@@ -11,6 +11,7 @@ import datetime
 import global_constants
 from send_webhooks import SendWebhooks
 
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
@@ -37,87 +38,72 @@ def main():
 
     service = build('gmail', 'v1', credentials=creds)
 
-    #response = service.users().labels().list(userId='me').execute()
-    #labels = response['labels']
-    #for label in labels:
-    #    print('Label id: %s - Label name: %s' % (label['id'], label['name']))
-
     # Call the Gmail API to return all unread emails
     results = service.users().messages().list(userId='me', labelIds='UNREAD').execute()
     messages = results.get('messages', [])
 
-    if not messages:
-        print('No unread messages found.')
-    else:
-        unread_new_turns = []
+    unread_new_turns = []
 
-        for message in messages:
-            # create a dictionary object reresenting the email
-            msg = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
+    for message in messages:
+        # create a dictionary object representing the email
+        msg = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
 
-            # retreave raw byte data from dictionary object
-            msg_bytes = base64.urlsafe_b64decode(msg['raw'].encode('ASCII'))
+        # retrieve raw byte data from dictionary object
+        msg_bytes = base64.urlsafe_b64decode(msg['raw'].encode('ASCII'))
 
-            # create message object from byte data
-            mime_msg = email.message_from_bytes(msg_bytes)
+        # create message object from byte data
+        mime_msg = email.message_from_bytes(msg_bytes)
 
-            # if the subject of the message is not the expected subject, do not pull data from it
-            if not mime_msg['Subject'] == global_constants.subject:
-                break
+        # if the subject of the message is not the expected subject, do not pull data from it
+        if not mime_msg['Subject'] == global_constants.subject:
+            break
 
-            # iterate over parts of the email object and store only the plain text of the email
-            if not mime_msg.is_multipart():
-                # retreave text of email part
+        # iterate over parts of the email object and store only the plain text of the email
+        if not mime_msg.is_multipart():
+            # retrieve text of email part
+            text = mime_msg.get_payload()
 
-                text = mime_msg.get_payload()
-                #print(text)
+            # store dictionary returned from pull_data in unread_new_turns
+            new_turn = pull_data(text)
+            unread_new_turns.append(new_turn)
 
-                # store dictionary returned from pull_data in unread_new_turns
-                new_turn = pull_data(text)
-                unread_new_turns.append(new_turn)
-                #print(new_turn)
-
-                # mark processed email as read
-                mark_as_read(service, message['id'])
-            else:
-                for part in mime_msg.walk():
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get('Content-Disposition'))
-
-                    # do not pull data from email part if it is not plain text with no attachments
-                    if content_type == 'text/plain' and 'attachment' not in content_disposition:
-                        # retreave text of email part
-                        text = part.get_payload()
-                        #print(text)
-
-                        # store dictionary returned from pull_data in unread_new_turns
-                        new_turn = pull_data(text)
-                        unread_new_turns.append(new_turn)
-                        #print(new_turn)
-
-                        # mark processed email as read
-                        mark_as_read(service, message['id'])
-                        break
-
-            #print("xxxxxxxxxxxxxxxx***next email***xxxxxxxxxxxxxxxx")
-
-        if unread_new_turns:
-            # create SendWebhook object and pass it all extracted data
-            sender = SendWebhooks(unread_new_turns)
-
-            # initiate send_all_new_turns and store tuple in sender_data
-            # sender data in the format: ((int)webhooks sent, (int)total webhooks)
-            turns_sent = sender.send_all_new_turns()
-
-            print("%s out of %s webhooks were sent at %s." % (turns_sent, len(sender.new_turns), datetime.datetime.now()))
-
-            if not turns_sent == len(sender.new_turns):
-                print("new turn that caused the error:")
-                print(sender.new_turns[turns_sent])
-            print("------------")
+            # mark processed email as read
+            mark_as_read(service, message['id'])
         else:
-            print("No new turns to send at %s." % datetime.datetime.now())
-            print("------------")
+            for part in mime_msg.walk():
+                content_type = part.get_content_type()
+                content_disposition = str(part.get('Content-Disposition'))
+
+                # do not pull data from email part if it is not plain text with no attachments
+                if content_type == 'text/plain' and 'attachment' not in content_disposition:
+                    # retrieve text of email part
+                    text = part.get_payload()
+
+                    # store dictionary returned from pull_data in unread_new_turns
+                    new_turn = pull_data(text)
+                    unread_new_turns.append(new_turn)
+
+                    # mark processed email as read
+                    mark_as_read(service, message['id'])
+                    break
+
+    if unread_new_turns:
+        # create SendWebhook object and pass it all extracted data
+        sender = SendWebhooks(unread_new_turns)
+
+        # initiate send_all_new_turns and store tuple in sender_data
+        # sender data in the format: ((int)webhooks sent, (int)total webhooks)
+        turns_sent = sender.send_all_new_turns()
+
+        print("%s out of %s webhooks were sent at %s." % (turns_sent, len(sender.new_turns), datetime.datetime.now()))
+
+        if not turns_sent == len(sender.new_turns):
+            print("new turn that caused the error:")
+            print(sender.new_turns[turns_sent])
+        print("------------")
+    else:
+        print("No new turns to send at %s." % datetime.datetime.now())
+        print("------------")
 
 
 # returns dictionary data after given fields
@@ -125,7 +111,7 @@ def main():
 #               string_key.game:game_name,
 #               string_key.player:player_name,
 #               string_key.turn:turn_number}
-def pull_data(str):
+def pull_data(email_text):
     fields_to_find = [global_constants.when, global_constants.game, global_constants.player, global_constants.turn]
     found_data = {}
     i = 0
@@ -133,17 +119,20 @@ def pull_data(str):
         # find index of the field starting at last point in the email string
         # raise an error if the field is not found
         try:
-            i = str.index(field, i)
+            i = email_text.index(field, i)
         except ValueError:
             raise ValueError("Field not found within string after given starting point")
+
         # set index to the first character after the field
         i = i + len(field)
+
         # store the string after the field and store it within found_data with the field as the key
         data = ""
-        while not str[i] == '\r':
-            data += str[i]
+        while not email_text[i] == '\r':
+            data += email_text[i]
             i += 1
         found_data[field] = data
+
     # raise an error if found_data is not the same size as fields_to_find
     assert (len(found_data) == len(fields_to_find))
     return found_data
